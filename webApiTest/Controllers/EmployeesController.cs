1d9f0074565b255ed42d6c460dc1b9b29e5506c1
HajerWael <hajerwael95@gmail.com>
2020-04-15 22:20:27 +0300

    Initial check-in of module webApiTest

---
+++ webApiTest/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+using System.Net.Http;
+using webApiTest.Models;
+using System.Collections.Generic;
+
+namespace webApiTest.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        static List<Employee> emps = new List<Employee>{
+            new Employee { Id = 1 , name = "Emp -1-" , age = 20 },
+            new Employee { Id = 2 , name = "Emp -2-" , age = 29 },
+            new Employee { Id = 3 , name = "Emp -3-" , age = 32 },
+            new Employee { Id = 4 , name = "Emp -4-" , age = 36 }
+        };
+
+        public List<Employee> Get(){
+            return emps;
+        }
+    }
+}
---
+++ webApiTest/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace webApiTest.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public Employee()
+        {
+        }
+    }
+}
